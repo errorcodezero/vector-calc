ab5d19bc31861405b7a527dcabf5cea5c1fa7979
ErrorCode0 <me@errorcodezero.dev>
2025-02-24 09:00:32 -0800

    Adding add and subtract

--- src/args.rs
+++ src/args.rs
@@ -12,4 +12,20 @@ pub enum Args {
         x: f32,
         y: f32,
     },
+    Add {
+        #[clap(long, short, action=ArgAction::SetFalse)]
+        radians: bool,
+        vector1: f32,
+        angle1: f32,
+        vector2: f32,
+        angle2: f32,
+    },
+    Subtract {
+        #[clap(long, short, action=ArgAction::SetFalse)]
+        radians: bool,
+        vector1: f32,
+        angle1: f32,
+        vector2: f32,
+        angle2: f32,
+    },
 }
--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,65 @@ fn main() {
             println!("{x}, {y}")
         }
         Args::Combine { x, y } => {
-            let v = (x.powf(2.0) + y.powf(2.0)).sqrt();
+            let v = (x.powi(2) + y.powi(2)).sqrt();
             println!("{}", v);
+        }
+        Args::Add {
+            vector1,
+            angle1,
+            vector2,
+            angle2,
+            radians,
+        } => {
+            let mut angle1 = angle1;
+            let mut angle2 = angle2;
+            if radians {
+                angle1 /= 180.0;
+                angle1 *= PI;
+
+                angle2 /= 180.0;
+                angle2 *= PI;
+            }
+            let x1 = angle1.cos() * vector1;
+            let y1 = angle1.sin() * vector1;
+
+            let x2 = angle2.cos() * vector2;
+            let y2 = angle2.sin() * vector2;
+
+            let x3 = x1 + x2;
+            let y3 = y1 + y2;
+            let v = (x3.powi(2) + y3.powi(2)).sqrt();
+
+            println!("x: {x3}, y:{y3}, final vector: {v}")
+        }
+        Args::Subtract {
+            vector1,
+            angle1,
+            vector2,
+            angle2,
+            radians,
+        } => {
+            let mut angle1 = angle1;
+            let mut angle2 = angle2;
+            if radians {
+                angle1 /= 180.0;
+                angle1 *= PI;
+
+                angle2 /= 180.0;
+                angle2 *= PI;
+            }
+
+            let x1 = angle1.cos() * vector1;
+            let y1 = angle1.sin() * vector1;
+
+            let x2 = angle2.cos() * vector2 * -1.0;
+            let y2 = angle2.sin() * vector2 * -1.0;
+
+            let x3 = x1 + x2;
+            let y3 = y1 + y2;
+            let v = (x3.powi(2) + y3.powi(2)).sqrt();
+
+            println!("x: {x3}, y:{y3}, final vector: {v}")
         }
     }
 }
